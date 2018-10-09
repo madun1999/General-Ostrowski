e79da7e4f3875459b33d61866057ec899e340661
Erik Joan Hernandez <erikjoanhernandez@wirelessprv-10-195-155-196.near.illinois.edu>
2018-10-09 11:36:44 -0500

    added constructer

--- Alg2Transition.java
+++ Alg2Transition.java
@@ -1,3 +1,21 @@
 static class Alg2Transition {
-
+    // Will construct a transition for an automaton
+    public int a = 0, b = 0, state = 0;
+    
+    public Alg2Transition(int a, int b, int state){
+	setTransitions(a, b, state);
+    }
+    
+    public void setTransitions(int a_, int b_, int state_){
+	if (0<= a_ && a_<=2) a = a_;
+	else a = -1; 
+	if (0<= b_ && b_<=2) b = b_;
+	else b = -1;
+	if (0 <= state_ 0<= 81) state = state_;
+	else state = -1;
+    }
+    
+    public String toString(int a, int b, state){
+	return Integer.toString(a) + Integer.toString(b) + " -> " + Integer.toString(state) + "\n";
+    }
 }
