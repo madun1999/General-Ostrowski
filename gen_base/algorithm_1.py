2b15d7121ae5d4dc45ace8b8eb2b4c909a2c92f5
Your Name <you@example.com>
2018-11-11 17:33:39 -0600

    directory for general base

---
+++ gen_base/algorithm_1.py
@@ -0,0 +1,5 @@
+from __future__ import print_function
+
+
+class alg1:
+    def __init__(self, total)
---
+++ gen_base/main.py
@@ -0,0 +1,17 @@
+import forward
+
+'''
+This calls our foward
+pass for addition of two
+Ostrowski numbers.
+TODO : 
+- generalize to arbitrary basis
+- add user input
+'''
+
+def main():
+    forward.main()
+
+if __name__=='__main__':
+    main()
+    
