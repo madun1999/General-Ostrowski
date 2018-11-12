e91e2666f07a16df10f70a6fb65f8dd8cd9c1555
Eric Ma <dunma2@illinois.edu>
2018-11-12 17:18:40 -0600

    Update Alg0

--- Alg0.java
+++ Alg0.java
@@ -1,23 +1,49 @@
 import java.lang.StringBuilder;
+
+/**
+ * This class is a generator for algorithm 0.
+ *
+ * Requirement:
+ * allStatesToString(int max) should return a StringBuilder representing algorithm 0.
+ * For example, for rt2, since the max=2, the returned string should look like:
+ *
+ * {0,1,2} {0,1,2} {0,1,2,3,4}
+ * 0 1
+ * 0 0 -> 0
+ * 0 1 -> 0
+ * 0 2 -> 0
+ * 1 0 -> 0
+ * 1 1 -> 0
+ * 1 2 -> 0
+ * 2 0 -> 0
+ * 2 1 -> 0
+ * 2 2 -> 0
+ *
+ */
 class Alg0 {
-    private static final int MAX = 2;
-    public static StringBuilder allStatesToString() {
+    /**
+     * Return a StringBuilder representing algorithm 0.
+     * Read the class description.
+     * @return StringBuilder for the state.
+     */
+    public static StringBuilder allStatesToString(int max) {
         StringBuilder s = new StringBuilder();
         s.append("{0,1,2} {0,1,2} {0,1,2,3,4}");
         s.append("\n");
         s.append("0 1");
         s.append("\n");
-        for (int i = 0;i<=MAX ;i++ ) {
-            for (int j=0;j<=MAX ;j++ ) {
-                s.append("" + i + " " + j + " " + (i+j) + " -> " + "0");
+        for (int i = 0;i<=max ;i++ ) {
+            for (int j=0;j<=max ;j++ ) {
+                s.append(i);
+                s.append(" ");
+                s.append(j);
+                s.append(" ");
+                s.append(i+j);
+                s.append(" -> 0");
                 s.append("\n");
             }
         }
         return s;
     }
-
-    //interchange statenumber and entries
-    public static int getStateNumber(int[] entries) {Alg1State temp = new Alg1State(entries);return temp.getStateNumber();}
-    public static int[] getEntries(int stateNumber) {Alg1State temp = new Alg1State(stateNumber);return temp.getEntries();}
 
 }
