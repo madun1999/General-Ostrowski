54e861a61555bc4d1ff1bac33e0bccdc45244a08
Eric Ma <dunma2@illinois.edu>
2018-11-12 21:10:14 -0600

    Finish Alg0

--- Alg0.java
+++ Alg0.java
@@ -24,16 +24,34 @@ class Alg0 {
     /**
      * Return a StringBuilder representing algorithm 0.
      * Read the class description.
+     * @param max The maximum number the input can be.
      * @return StringBuilder for the state.
      */
     public static StringBuilder allStatesToString(int max) {
         StringBuilder s = new StringBuilder();
-        s.append("{0,1,2} {0,1,2} {0,1,2,3,4}");
+        for (int j = 0; j < 2; j++) {
+            s.append("{");
+            for (int i = 0; i < max; i++) {
+                s.append(i);
+                s.append(",");
+            }
+            s.append(max);
+            s.append("}");
+            s.append(" ");
+        }
+
+        s.append("{");
+        for (int i = 0; i < max*2; i++) {
+            s.append(i);
+            s.append(",");
+        }
+        s.append(max*2);
+        s.append("}");
         s.append("\n");
         s.append("0 1");
         s.append("\n");
-        for (int i = 0;i<=max ;i++ ) {
-            for (int j=0;j<=max ;j++ ) {
+        for (int i = 0;i<=max;i++ ) {
+            for (int j=0;j<=max;j++ ) {
                 s.append(i);
                 s.append(" ");
                 s.append(j);
