db5b56b3e4620fa943ad32b5848885ab7c9c41e6
jtg2 <jtg2@illinois.edu>
2018-11-21 17:38:07 -0600

    Support systems with non-repeating component

--- Recognition.java
+++ Recognition.java
@@ -1,22 +1,33 @@
 import java.lang.StringBuilder;
 
 public class Recognition {
-    public Recognition(int[] bases, int maxBase) {
-        BASES = bases;
-        MAX_BASE = maxBase;
-    }
 
-    //BASES holds the q values for one period of an Ostrowski expansion
-    private int[] BASES;
-    //MAX_BASE holds the largest q value, for generating alphabet
+    //NON_REP_BASES holds the a values for non-repeating part of continued fraction expansion
+    private int[] NON_REP_BASES;
+    //REP_BASES holds the a values for one period of continued fraction expansion
+    private int[] REP_BASES;
+    //MAX_BASE holds the largest a value, for generating alphabet
     private int MAX_BASE;
-    //todo add arrays to hold repeating portion of bases and non repeated separately
+
+    public Recognition(int[] nonRepBases, int[] repBases) {
+        NON_REP_BASES = nonRepBases;
+        REP_BASES = repBases;
+        int max = 0;
+        for (int a : nonRepBases)
+            if (a > max)
+                max = a;
+        for (int a : repBases)
+            if (a > max)
+                max = a;
+        MAX_BASE = max;
+    }
 
     //generates automaton text
     public StringBuilder generate() {
         StringBuilder s = new StringBuilder();
-        //see notes for overview, 2n+1 states total, n=BASES.length
-        int n = BASES.length;
+        //see notes for overview, 2n+1 states total, n = total number of base values
+        int start = NON_REP_BASES.length + 1;
+        int n = REP_BASES.length + start - 1;
         s.append("{0");
         for (int i = 1; i <= MAX_BASE; i++) {
             s.append(",").append(i);
@@ -24,30 +35,46 @@ public class Recognition {
         s.append("}\n");
         //every number system starts with this behavior
         s.append("0 1\n").append("0 -> 1\n1 -> ").append(n + 1).append("\n");
-        //i is index of the a value in BASES + 1
-        //loop goes through all bases except the last, since those point to the first pair of states
-        for (int i = 1; i < n; i++) {
+        //i is index of the a value in REP_BASES + 1
+
+        //non-repeating part of automaton
+        for (int i = 1; i < start; i++) {
             s.append(i).append(" 1\n");
             s.append("0 -> ").append(i+1).append("\n");
-            for (int k = 1; k <= BASES[i-1]; k++) {
+            for (int k = 1; k < NON_REP_BASES[i-1]; k++) {
+                s.append(k).append(" -> ").append(i+n+1).append("\n");
+            }
+            s.append(i+n).append(" 1\n");
+            s.append("0 -> ").append(i+1).append("\n");
+            for (int k = 1; k < NON_REP_BASES[i-1]; k++) {
+                s.append(k).append(" -> ").append(i+n+1).append("\n");
+            }
+        }
+
+        //repeating part of automaton
+        //loop goes through all bases except the last, since those point to the first pair of states
+        for (int i = start; i < n; i++) {
+            s.append(i).append(" 1\n");
+            s.append("0 -> ").append(i+1).append("\n");
+            for (int k = 1; k <= REP_BASES[i-start]; k++) {
                 s.append(k).append(" -> ").append(i + n + 1).append("\n");
             }
             s.append(i + n).append(" 1\n");
             s.append("0 -> ").append(i + 1).append("\n");
-            for (int k = 1; k < BASES[i-1]; k++) {
+            for (int k = 1; k < REP_BASES[i-start]; k++) {
                 s.append(k).append(" -> ").append(i + n + 1).append("\n");
             }
         }
 
         s.append(n).append(" 1\n");
-        s.append("0 -> 1\n");
-        for (int k = 1; k <= BASES[n-1]; k++) {
-            s.append(k).append(" -> ").append(n + 1).append("\n");
+        s.append("0 -> ").append(start).append("\n");
+        for (int k = 1; k <= REP_BASES[REP_BASES.length-1]; k++) {
+            s.append(k).append(" -> ").append(n + start).append("\n");
         }
         s.append(2*n).append(" 1\n");
-        s.append("0 -> 1\n");
-        for (int k = 1; k < BASES[n-1]; k++) {
-            s.append(k).append(" -> ").append(n + 1).append("\n");
+        s.append("0 -> ").append(start).append("\n");
+        for (int k = 1; k < REP_BASES[REP_BASES.length-1]; k++) {
+            s.append(k).append(" -> ").append(n + start).append("\n");
         }
 
         return s;
