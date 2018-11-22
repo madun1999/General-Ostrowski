6962cc251ba314e3fd92c6b05fc169868e02f639
Eric Ma <dunma2@illinois.edu>
2018-11-21 19:57:46 -0600

    Change Output File Directory

--- src/ostrowski/OstrowskiAddition.java
+++ src/ostrowski/OstrowskiAddition.java
@@ -3,6 +3,7 @@ package ostrowski;
 import java.lang.Exception;
 import java.io.BufferedWriter;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.charset.StandardCharsets;
 
@@ -11,6 +12,7 @@ public class OstrowskiAddition {
         if (args.length>=1){
             return;
         }
+        String name = "rt2";
         int[] a = {2};
         int nonRepeatLength = 0;
 
@@ -19,28 +21,35 @@ public class OstrowskiAddition {
         Alg2Automaton alg2 = new Alg2Automaton(a,nonRepeatLength);
         Alg3Automaton alg3 = new Alg3Automaton(a,nonRepeatLength);
 
-        BufferedWriter writer = null;
+        String directory = "./output/"+name;
+        Path path = Paths.get(directory);
+        Path alg0Path = Paths.get(directory+"/alg0.txt");
+        Path alg1Path = Paths.get(directory+"/alg1.txt");
+        Path alg2Path = Paths.get(directory+"/alg2.txt");
+        Path alg3Path = Paths.get(directory+"/alg3.txt");
+        BufferedWriter writer;
         try{
-            if (Files.exists(Paths.get("./output/alg0.txt"))) Files.delete(Paths.get("./output/alg0.txt"));
-            if (Files.exists(Paths.get("./output/alg1.txt"))) Files.delete(Paths.get("./output/alg1.txt"));
-            if (Files.exists(Paths.get("./output/alg2.txt"))) Files.delete(Paths.get("./output/alg2.txt"));
-            if (Files.exists(Paths.get("./output/alg3.txt"))) Files.delete(Paths.get("./output/alg3.txt"));
+            if (!Files.exists(path)) Files.createDirectory(path);
+            if (Files.exists(alg0Path)) Files.delete(alg0Path);
+            if (Files.exists(alg1Path)) Files.delete(alg1Path);
+            if (Files.exists(alg2Path)) Files.delete(alg2Path);
+            if (Files.exists(alg3Path)) Files.delete(alg3Path);
 
-            writer = Files.newBufferedWriter(Paths.get("./output/alg0.txt"), StandardCharsets.UTF_16);
+            writer = Files.newBufferedWriter(alg0Path, StandardCharsets.UTF_16);
             writer.append(alg0.toStringBuilder());
             writer.close();
 
-            writer = Files.newBufferedWriter(Paths.get("./output/alg1.txt"), StandardCharsets.UTF_16);
+            writer = Files.newBufferedWriter(alg1Path, StandardCharsets.UTF_16);
             alg1.calculateAutomaton();
             writer.append(alg1.toStringBuilder());
             writer.close();
 
-            writer = Files.newBufferedWriter(Paths.get("./output/alg2.txt"), StandardCharsets.UTF_16);
+            writer = Files.newBufferedWriter(alg2Path, StandardCharsets.UTF_16);
             alg2.calculateAutomaton();
             writer.append(alg2.toStringBuilder());
             writer.close();
 
-            writer = Files.newBufferedWriter(Paths.get("./output/alg3.txt"), StandardCharsets.UTF_16);
+            writer = Files.newBufferedWriter(alg3Path, StandardCharsets.UTF_16);
             alg3.calculateAutomaton();
             writer.append(alg3.toStringBuilder());
             writer.close();
